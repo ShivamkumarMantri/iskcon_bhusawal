3a0b808c7e3917ea20e01d96200d2e99a7b14f81
ShivamkumarMantri <shivammantri0020@gmail.com>
2026-02-05 19:24:38 +0530

    scroll to top

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,16 @@ import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop.jsx';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Header />
-      <App />
+      <ScrollToTop>
+        <App />
+      </ScrollToTop>
       <Footer />
     </BrowserRouter>
   </StrictMode>,
